082a0c6ce9b48dde7d1a7a74f793a437e0885ee7
dar-b-r-b <d.tausheva9@gmail.com>
2024-10-29 11:13:45 +0300

    added density calculation for three yarns

--- DensityCalc.js
+++ DensityCalc.js
@@ -8,18 +8,30 @@ export function DensityCalc() {
   const [yarns, setYarns] = useState([
     { id: 1, name: `Нить 1`, length: null, weight: null },
     { id: 2, name: `Нить 2`, length: null, weight: null },
+    { id: 3, name: `Нить 3`, length: null, weight: null },
   ]);
 
   const [result, setResult] = useState(0);
   const _calcDensity = ([
     { weight: weight1, length: length1 },
     { weight: weight2, length: length2 },
+    { weight: weight3, length: length3 },
   ]) => {
     let coef1 = 100 / weight1;
     let coef2 = 100 / weight2;
 
+    if (weight3 === null && length3 === null) {
+      return Math.ceil(
+        (length1 * coef1 * length2 * coef2) /
+          (length1 * coef1 + length2 * coef2)
+      );
+    }
+    let coef3 = 100 / weight3;
     return Math.ceil(
-      (length1 * coef1 * length2 * coef2) / (length1 * coef1 + length2 * coef2)
+      (length1 * coef1 * length2 * coef2 * length3 * coef3) /
+        (length1 * coef1 * length2 * coef2 +
+          length2 * coef2 * length3 * coef3 +
+          length1 * coef1 * length3 * coef3)
     );
   };
 
